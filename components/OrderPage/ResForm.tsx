703581d8fe5e0410f3307accfaa50ecf14c2d2b9
David Kapteyn <dave.kapteyn@gmail.com>
2022-10-13 20:53:00 -0600

    added form validation

--- components/OrderPage/ResForm.tsx
+++ components/OrderPage/ResForm.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useReducer } from "react";
 import s from "../../styles/OrderPage/ResForm.module.scss";
 import Button from "../shared/Button";
 export default function ResForm() {
@@ -6,8 +6,12 @@ export default function ResForm() {
   const [dropDown, setDropDown] = useState(false);
   const [AMPM, setAMPM] = useState("AM");
   const [checked, setChecked] = useState(s.checked1);
+  const [nameError, setNameError] = useState("");
+  const [emailError, setEmailError] = useState("");
+  const [dateError, setDateError] = useState("");
+  const [timeError, setTimeError] = useState("");
 
-  const initialValue = {
+  const [formValues, setFormValues] = useState({
     name: "",
     email: "",
     year: "",
@@ -15,8 +19,14 @@ export default function ResForm() {
     day: "",
     hour: "",
     minute: "",
-  };
-  const [formValues, setFormValues] = useState(initialValue);
+  });
+
+  const [errorValue, setErrorValue] = useState({
+    name: "",
+    email: "",
+    date: "",
+    time: "",
+  });
 
   function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
     const { name, value } = e.target;
@@ -33,31 +43,106 @@ export default function ResForm() {
 
   function validate(e: React.FormEvent) {
     e.preventDefault();
-    console.log({ ...formValues });
+    let errorSubmit = {
+      name: "",
+      email: "",
+      date: "",
+      time: "",
+    };
+    //Name validation
+    if (formValues.name === "") {
+      errorSubmit.name = "This field is Required";
+      setNameError(s.inputError);
+    }
+
+    //email validation
+    let emailRegex =
+      /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+    formValues.email === ""
+      ? ((errorSubmit.email = "This field is required"),
+        setEmailError(s.inputError))
+      : emailRegex.test(formValues.email) === false
+      ? ((errorSubmit.email = "valid email Required"),
+        setEmailError(s.inputError))
+      : (errorSubmit.email = "");
+
+    formValues.month === "" || formValues.day === "" || formValues.year === ""
+      ? ((errorSubmit.date = "This field is incomplete"),
+        setDateError(s.inputError))
+      : /[1-31]/.test(formValues.day) ||
+        /[1-12]/.test(formValues.month) ||
+        formValues.year.length > 4
+      ? ((errorSubmit.date = "Valid Date Required"), setDateError(s.inputError))
+      : (errorSubmit.date = "");
+
+    if (formValues.hour === "" || formValues.minute === "") {
+      (errorSubmit.time = "This field is incomplete"),
+        setTimeError(s.inputError);
+    }
+    setErrorValue({ ...errorSubmit });
+  }
+
+  function resetNameError() {
+    setErrorValue({ ...errorValue, name: "" });
+    setNameError("");
+  }
+
+  function resetEmailError() {
+    setErrorValue({ ...errorValue, email: "" });
+    setEmailError("");
+  }
+  function resetDateError() {
+    setErrorValue({ ...errorValue, date: "" });
+    setDateError("");
+  }
+
+  function resetTimeError() {
+    setErrorValue({ ...errorValue, time: "" });
+    setTimeError("");
   }
 
   return (
     <div className={s.mainContainer}>
       <form className={s.formContainer} action="post" onSubmit={validate}>
-        <input
-          className={s.inText}
-          type="text"
-          placeholder="Name"
-          onChange={handleChange}
-          name="name"
-        />
-        <input
-          className={`${s.inText} ${s.inTextEmail}`}
-          type="text"
-          placeholder="Email"
-          onChange={handleChange}
-          name="email"
-        />
+        <div
+          onClick={resetNameError}
+          className={`${s.TAndEContainer} ${nameError}`}
+        >
+          <input
+            className={`${s.inputError} ${s.inText}  `}
+            type="text"
+            placeholder="Name"
+            onChange={handleChange}
+            name="name"
+          />
+          <p className={`${s.error} ${s.errorName}`}>{errorValue.name}</p>
+        </div>
+        <div
+          onClick={resetEmailError}
+          className={`${s.TAndEContainer} ${emailError}`}
+        >
+          <input
+            className={`${s.inText} ${s.inTextEmail}`}
+            type="text"
+            placeholder="Email"
+            onChange={handleChange}
+            name="email"
+          />
+          <p className={`${s.error} ${s.errorEmail}`}>{errorValue.email}</p>
+        </div>
         <div className={s.inDateContainer}>
-          <div className={s.inDateAndText}>
-            <h4 className={s.DateTitle}>Pick a date</h4>
-
-            <div className={s.inDates}>
+          <div
+            onClick={resetDateError}
+            className={`${s.inDateAndText} ${dateError}`}
+          >
+            <div className={s.TAndEContainer}>
+              <h4 className={s.DateTitle}>Pick a date</h4>
+              <p className={`${s.error} ${s.errorDateTime}`}>
+                {errorValue.date}
+              </p>
+            </div>
+            <div className={`${s.inDates} ${dateError}`}>
               <input
                 className={s.inDate}
                 type="text"
@@ -82,10 +167,15 @@ export default function ResForm() {
             </div>
           </div>
         </div>
-        <div className={s.inDateContainer}>
-          <div className={s.inDateAndText}>
-            <h4 className={s.DateTitle}>Pick a time</h4>
-            <div className={s.inDates}>
+        <div onClick={resetTimeError} className={s.inDateContainer}>
+          <div className={`${s.inDateAndText} ${timeError}`}>
+            <div className={s.TAndEContainer}>
+              <h4 className={s.DateTitle}>Pick a time</h4>
+              <p className={`${s.error} ${s.errorDateTime}`}>
+                {errorValue.time}
+              </p>
+            </div>
+            <div className={`${s.inDates} ${timeError}`}>
               <input
                 className={s.inDate}
                 name="hour"
